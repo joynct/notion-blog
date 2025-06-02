ea1783fcb76388dfeab606b1b3bc7acfa03e3dcb
joynct <joyceaniceto_rv@gmail.com>
2025-06-02 20:15:44 -0300

    Atualização no footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <span>Deploy your own!</span>
+        <span>Crie seu blog</span>
         <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
           <img
             src="https://vercel.com/button"
@@ -14,10 +14,7 @@ export default function Footer() {
           />
         </ExtLink>
         <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
+          or <ExtLink href="https://criajoy.netlify.app/">view source</ExtLink>
         </span>
       </footer>
     </>
