b39544cdb09fd195500cd5b6ac3b224db4d5053f
joynct <joyceaniceto_rv@gmail.com>
2025-07-24 18:22:25 -0300

    feat: Alterações gerais e atualização na página de contato

--- scripts/create-table.js
+++ scripts/create-table.js
@@ -1,3 +1,14 @@
 const main = require('../src/lib/notion/createTable')
 
 main()
+import { getAllPosts } from '../lib/notion'
+
+async function main() {
+  const posts = await getAllPosts()
+  console.log(posts)
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
