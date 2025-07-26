7c11ba88a0d5181f7b079104b232ab8e8d0f30fb
joynct <joyceaniceto_rv@gmail.com>
2025-07-26 20:05:54 -0300

    melhorias de layout e correção do authors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,28 +8,22 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <img
-          src="/clog criativo.png"
-          height="85"
-          width="250"
-          alt="Clog Criativo"
-        />
-        <h1>Clog Criativo</h1>
+        <img src="/clog criativo.png" width="400" alt="Clog Criativo" />
         <h2>Conhecimento nunca é demais. </h2>
 
         <div className="explanation">
           <p>
+            {' '}
             Aqui, você encontra ideias, reflexões e soluções que unem
             criatividade e resultado. Um espaço para quem quer vender no digital
             com mais clareza, consistência e personalidade.
-            <p>
-              Esse espaço nasceu para compartilhar o que aprendo e aplico
-              diariamente com marcas reais: estratégias que funcionam, design
-              que vende e marketing que respeita o tempo (e o bolso) de quem
-              está do outro lado. Conheça mais sobre o meu trabalho acessando
-              por aqui{' '}
-              <ExtLink href="https://criajoy.netlify.app/">criajoy</ExtLink>
-            </p>
+          </p>
+          <div style={{ height: '20px' }}></div>
+          <p>
+            Esse espaço nasceu para compartilhar o que aprendo e aplico
+            diariamente com marcas reais: estratégias que funcionam, design que
+            vende e marketing que respeita o tempo (e o bolso) de quem está do
+            outro lado.
           </p>
         </div>
       </div>
